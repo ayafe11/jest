ed98311898b584d21f3de841e84de3638e5dd296
aya mostafa <ayamost@banquemisr.com>
2024-12-14 16:37:02 +0200

    test: add jest async/await samples

--- src/__tests__/2-asynchronous.test.js
+++ src/__tests__/2-asynchronous.test.js
@@ -1,6 +1,30 @@
-describe('Testing Asynchronous Code',()=>{
-//**Promises**//
-test('Promises',()=>{
+const expectedApiResponse = {
+    "name": "aya",
+    "department": "Software Development"
+}
+describe('Testing Asynchronous Code', () => {
+    function fetchData() {
+        return fetch('http://127.0.0.1:8081/data.json').then(res => res.json()).then((data) => data);
+    }
+    function fetchDataWithError() {
+      return new Promise((resolve,reject)=>{
+          reject('error');
+      })
 
-})
+    }
+    //!**Promises**!//
+    test('Promises', () => {
+        return fetchData().then(data => {
+            expect(data).toEqual(expectedApiResponse);
+        });
+    })
+    //**Async/ Await**//
+    test('Async/Await', async () => {
+        const data = await fetchData();
+        expect(data).toEqual(expectedApiResponse);
+        //or
+        await expect(fetchData()).resolves.toEqual(expectedApiResponse);
+        expect(fetchDataWithError()).rejects.toMatch('error');
+
+    })
 })
